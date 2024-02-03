4094261f2a19d272858232bb1a70a2c2201f663c
tangramor <tanramor@gmail.com>
2024-02-03 13:42:59 +0800

    working on preview...

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import { useTranslation } from 'react-i18next';
 import { open } from "@tauri-apps/api/dialog";
 import { getFileNameFromPath } from "./helper";
 import { ZipXmind } from "./ZipXmind";
+import { XMindEmbedViewer } from "xmind-embed-viewer";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
   const [xmindFilePath, setXmindFilePath] = useState("");
   const [isZipView, setIsZipView] = useState(false)
+  const [showPreview, setShowPreview] = useState(false)
   const { t } = useTranslation();
 
   async function extract() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("extract", { name }));
   }
-  
+
   var fileXmind;
   async function openXmindFile() {
     fileXmind = await open({
@@ -34,50 +36,78 @@ function App() {
     }
   }
 
+  async function preview() {
+    // const res = await fetch(name)
+    const viewer = new XMindEmbedViewer({
+      el: '#previewContainer',
+      region: 'cn',
+      file: await (await fetch(name)).arrayBuffer()
+    });
+    console.log("load xmind " + name);
+
+    // viewer.load(await res.arrayBuffer());
+    // fetch(xmindFilePath)
+    //   .then(res => res.arrayBuffer())
+    //   .then(file => viewer.load(file))
+  }
+
   return (
-    <div className="container">
-      <h1>{t('xmindFileExtractOrCompress')}</h1>
-
-      <p>{t('clickIconToSwitch')}</p>
-
-      <div className="row">
-        <a onClick={() => setIsZipView(!isZipView)} >
-          <img src="/xmind-extractor.svg" className="logo xmind-extractor" alt="Vite logo" />
-        </a>
-      </div>
-
-      {isZipView ? (
-        <ZipXmind />
-      ) : (
+    <>
+      {showPreview ? (
         <div>
-          <p>{t('descExtractXmind')}</p>
+          <div className="preview">
+            <div id="previewContainer"></div>
+          </div>
+          <button className="hideBtn" onClick={() => setShowPreview(false)}>Hide Preview</button>
+        </div>
+      ) : (
+        <div className="container">
 
-          <form
-            className="col"
+          <h1>{t('xmindFileExtractOrCompress')}</h1>
 
-          >
-            <input
-              id="greet-input"
-              onClick={() => openXmindFile()}
-              placeholder={t('inputName')}
-              value={xmindFilePath}
-            />
-            <p></p>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                extract();
-              }}>{t('extract')}</button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-              }}>{t('preview')}</button>
-          </form>
+          <p>{t('clickIconToSwitch')}</p>
 
-          <p>{greetMsg}</p>
+          <div className="row">
+            <a onClick={() => setIsZipView(!isZipView)} >
+              <img src="/xmind-extractor.svg" className="logo xmind-extractor" alt="Vite logo" />
+            </a>
+          </div>
+
+          {isZipView ? (
+            <ZipXmind />
+          ) : (
+            <div>
+              <p>{t('descExtractXmind')}</p>
+
+              <form
+                className="col"
+              >
+                <input
+                  id="greet-input"
+                  onClick={() => openXmindFile()}
+                  placeholder={t('inputName')}
+                  value={xmindFilePath}
+                />
+                <p></p>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    extract();
+                  }}>{t('extract')}</button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowPreview(true);
+                    preview();
+                  }}>{t('preview')}</button>
+              </form>
+
+              <p>{greetMsg}</p>
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 }
 
